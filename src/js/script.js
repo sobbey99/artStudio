dea06fc9fcfabf6b9686ae7baa77780669d559cf
sobbey99 <sobbey99@gmail.com>
2021-06-20 18:30:52 +0300

    Finished until FAQ section

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    //SLICK SLIDER
+    $('.forwhom-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/slider-prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/slider-next.svg"></button>'
+    });
+});
